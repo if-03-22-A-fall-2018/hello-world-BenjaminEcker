f189d19ed88cc97ddf0350fa6a80f76aea39fdeb
BenjaminEcker <43369801+BenjaminEcker@users.noreply.github.com>
2018-09-18 11:12:36 +0200

    Add files via upload

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,11 @@
+/****************************
+* Autor: Benjamin Ecker
+* Datum: 17.09.2018
+****************************/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello world\n");
+  printf("Hello World\n");
   return 0;
 }
